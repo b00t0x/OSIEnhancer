7ca6b4f983ea3914a8df89ad2911d6cff086c780
b00t0x <2325377+b00t0x@users.noreply.github.com>
2023-11-23 01:52:10 +0900

    [skip ci] fix indentation

--- OSIEnhancer/interfaces.hpp
+++ OSIEnhancer/interfaces.hpp
@@ -2,47 +2,47 @@ typedef uint32_t acpi_status;
 typedef const char *acpi_string;
 
 typedef struct {
-	acpi_string osiString;
-	int minKernel;
-	int maxKernel;
+    acpi_string osiString;
+    int minKernel;
+    int maxKernel;
 } interface_info_t;
 
 interface_info_t interfaces[] = {
-  // common os name
-  {"macOS"},
-  // macOS by name
-  // {"Tiger",         KernelVersion::Tiger},
-  // {"Leopard",       KernelVersion::Leopard},
-  // {"Snow Leopard",  KernelVersion::SnowLeopard},
-  // {"Lion",          KernelVersion::Lion},
-  // {"Mountain Lion", KernelVersion::MountainLion},
-  // {"Mavericks",     KernelVersion::Mavericks},
-  {"Yosemite",      KernelVersion::Yosemite},
-  {"El Capitan",    KernelVersion::ElCapitan},
-  {"Sierra",        KernelVersion::Sierra},
-  {"High Sierra",   KernelVersion::HighSierra},
-  {"Mojave",        KernelVersion::Mojave},
-  {"Catalina",      KernelVersion::Catalina},
-  {"Big Sur",       KernelVersion::BigSur},
-  {"Monterey",      KernelVersion::Monterey},
-  {"Ventura",       KernelVersion::Ventura},
-  {"Sonoma",        KernelVersion::Sonoma},
-  // macOS by version
-  // {"Mac OS X 10.4", KernelVersion::Tiger},
-  // {"Mac OS X 10.5", KernelVersion::Leopard},
-  // {"Mac OS X 10.6", KernelVersion::SnowLeopard},
-  // {"OS X 10.7",     KernelVersion::Lion},
-  // {"OS X 10.8",     KernelVersion::MountainLion},
-  // {"OS X 10.9",     KernelVersion::Mavericks},
-  {"OS X 10.10",    KernelVersion::Yosemite},
-  {"OS X 10.11",    KernelVersion::ElCapitan},
-  {"macOS 10.12",   KernelVersion::Sierra},
-  {"macOS 10.13",   KernelVersion::HighSierra},
-  {"macOS 10.14",   KernelVersion::Mojave},
-  {"macOS 10.15",   KernelVersion::Catalina},
-  // macOS 11 or later are auto generated
-  // interface by range examples
-  {"Skylake Framebuffer",     KernelVersion::ElCapitan, KernelVersion::Monterey},
-  {"Kaby Lake Framebuffer",   KernelVersion::Sierra, -1}, // possibly Sonoma is the last
-  {"Coffee Lake Framebuffer", KernelVersion::HighSierra, -1},
+    // common os name
+    {"macOS"},
+    // macOS by name
+    // {"Tiger",         KernelVersion::Tiger},
+    // {"Leopard",       KernelVersion::Leopard},
+    // {"Snow Leopard",  KernelVersion::SnowLeopard},
+    // {"Lion",          KernelVersion::Lion},
+    // {"Mountain Lion", KernelVersion::MountainLion},
+    // {"Mavericks",     KernelVersion::Mavericks},
+    {"Yosemite",      KernelVersion::Yosemite},
+    {"El Capitan",    KernelVersion::ElCapitan},
+    {"Sierra",        KernelVersion::Sierra},
+    {"High Sierra",   KernelVersion::HighSierra},
+    {"Mojave",        KernelVersion::Mojave},
+    {"Catalina",      KernelVersion::Catalina},
+    {"Big Sur",       KernelVersion::BigSur},
+    {"Monterey",      KernelVersion::Monterey},
+    {"Ventura",       KernelVersion::Ventura},
+    {"Sonoma",        KernelVersion::Sonoma},
+    // macOS by version
+    // {"Mac OS X 10.4", KernelVersion::Tiger},
+    // {"Mac OS X 10.5", KernelVersion::Leopard},
+    // {"Mac OS X 10.6", KernelVersion::SnowLeopard},
+    // {"OS X 10.7",     KernelVersion::Lion},
+    // {"OS X 10.8",     KernelVersion::MountainLion},
+    // {"OS X 10.9",     KernelVersion::Mavericks},
+    {"OS X 10.10",    KernelVersion::Yosemite},
+    {"OS X 10.11",    KernelVersion::ElCapitan},
+    {"macOS 10.12",   KernelVersion::Sierra},
+    {"macOS 10.13",   KernelVersion::HighSierra},
+    {"macOS 10.14",   KernelVersion::Mojave},
+    {"macOS 10.15",   KernelVersion::Catalina},
+    // macOS 11 or later are auto generated
+    // interface by range examples
+    {"Skylake Framebuffer",     KernelVersion::ElCapitan, KernelVersion::Monterey},
+    {"Kaby Lake Framebuffer",   KernelVersion::Sierra, -1}, // possibly Sonoma is the last
+    {"Coffee Lake Framebuffer", KernelVersion::HighSierra, -1},
 };
--- OSIEnhancer/kern_start.cpp
+++ OSIEnhancer/kern_start.cpp
@@ -7,62 +7,62 @@ extern "C" acpi_status AcpiInstallInterface(acpi_string);
 static int kernelVersion = getKernelVersion();
 
 static void installInterfaces() {
-	// defined in interfaces.h
-	for (int i = 0; i < sizeof(interfaces) / sizeof(interface_info_t); ++i) {
-		interface_info_t *info = &interfaces[i];
-		if (info->maxKernel == 0) {
-			info->maxKernel = info->minKernel;
-		}
-		if (info->minKernel > 0 && kernelVersion < info->minKernel) {
-			DBGLOG("osi", "Skipped _OSI(\"%s\") due to KernelVersion(%d) < %d", info->osiString, kernelVersion, info->minKernel);
-			continue;
-		}
-		if (info->maxKernel > 0 && kernelVersion > info->maxKernel) {
-			DBGLOG("osi", "Skipped _OSI(\"%s\") due to KernelVersion(%d) > %d", info->osiString, kernelVersion, info->maxKernel);
-			continue;
-		}
-		SYSLOG("osi", "Added _OSI(\"%s\")", info->osiString);
-		AcpiInstallInterface(info->osiString);
-	}
+    // defined in interfaces.h
+    for (int i = 0; i < sizeof(interfaces) / sizeof(interface_info_t); ++i) {
+        interface_info_t *info = &interfaces[i];
+        if (info->maxKernel == 0) {
+            info->maxKernel = info->minKernel;
+        }
+        if (info->minKernel > 0 && kernelVersion < info->minKernel) {
+            DBGLOG("osi", "Skipped _OSI(\"%s\") due to KernelVersion(%d) < %d", info->osiString, kernelVersion, info->minKernel);
+            continue;
+        }
+        if (info->maxKernel > 0 && kernelVersion > info->maxKernel) {
+            DBGLOG("osi", "Skipped _OSI(\"%s\") due to KernelVersion(%d) > %d", info->osiString, kernelVersion, info->maxKernel);
+            continue;
+        }
+        SYSLOG("osi", "Added _OSI(\"%s\")", info->osiString);
+        AcpiInstallInterface(info->osiString);
+    }
 
-	// _OSI("macOS xx")
-	if (kernelVersion >= KernelVersion::BigSur) {
-		char macosString[9];
-		snprintf(macosString, sizeof(macosString), "macOS %d", kernelVersion - 9);
-		SYSLOG("osi", "Added _OSI(\"%s\")", macosString);
-		AcpiInstallInterface(macosString);
-	}
+    // _OSI("macOS xx")
+    if (kernelVersion >= KernelVersion::BigSur) {
+        char macosString[9];
+        snprintf(macosString, sizeof(macosString), "macOS %d", kernelVersion - 9);
+        SYSLOG("osi", "Added _OSI(\"%s\")", macosString);
+        AcpiInstallInterface(macosString);
+    }
 
-	// _OSI("Darwin xx")
-	char darwinString[10];
-	snprintf(darwinString, sizeof(darwinString), "Darwin %d", kernelVersion);
-	SYSLOG("osi", "Added _OSI(\"%s\")", darwinString);
-	AcpiInstallInterface(darwinString);
+    // _OSI("Darwin xx")
+    char darwinString[10];
+    snprintf(darwinString, sizeof(darwinString), "Darwin %d", kernelVersion);
+    SYSLOG("osi", "Added _OSI(\"%s\")", darwinString);
+    AcpiInstallInterface(darwinString);
 }
 
 static const char *bootargOff[] {
-	"-osioff"
+    "-osioff"
 };
 
 static const char *bootargDebug[] {
-	"-osidbg"
+    "-osidbg"
 };
 
 static const char *bootargBeta[] {
-	"-osibeta"
+    "-osibeta"
 };
 
 PluginConfiguration ADDPR(config) {
-	xStringify(PRODUCT_NAME),
-	parseModuleVersion(xStringify(MODULE_VERSION)),
-	LiluAPI::AllowNormal,
-	bootargOff,
-	arrsize(bootargOff),
-	bootargDebug,
-	arrsize(bootargDebug),
-	bootargBeta,
-	arrsize(bootargBeta),
-	KernelVersion::Yosemite,
-	KernelVersion::Sonoma,
-	installInterfaces
+    xStringify(PRODUCT_NAME),
+    parseModuleVersion(xStringify(MODULE_VERSION)),
+    LiluAPI::AllowNormal,
+    bootargOff,
+    arrsize(bootargOff),
+    bootargDebug,
+    arrsize(bootargDebug),
+    bootargBeta,
+    arrsize(bootargBeta),
+    KernelVersion::Yosemite,
+    KernelVersion::Sonoma,
+    installInterfaces
 };
